f64a496533d84e21f1b4c2abc8019dcdb4eb72b3
rishianand39 <rishi.rn818@gamil.com>
2022-03-23 00:55:55 +0530

    half done

---
+++ Authentication/src/index.js
@@ -0,0 +1,20 @@
+const express=require("express");
+
+const app=express();
+app.use(express.json())
+
+const userController=require("./controllers/user.controller")
+const {register,login}=require("./controllers/auth.controller")
+const productController=require("./controllers/product.controller")
+
+
+app.use("/users",userController)
+
+app.post("/register", register)
+
+app.post("/login", login)
+app.use("/product",productController)
+
+
+
+module.exports=app;
