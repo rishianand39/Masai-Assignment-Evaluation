c3d3983d0887fa08f7fb179b6bcec24224df39e8
rishianand39 <rishi.rn818@gamil.com>
2022-03-25 00:05:08 +0530

    complete

--- Authentication/src/controllers/auth.controller.js
+++ Authentication/src/controllers/auth.controller.js
@@ -1,7 +1,7 @@
 
 const User=require("../models/user.model")
 var jwt = require('jsonwebtoken');
-require('dotenv').config()
+require('dotenv').config();
 
 const generateToken=(user)=>{
     return jwt.sign({user},process.env.SECRET_KEY )
